0d2648110216ad6622294cd993cf8c1c4fc52a3a
Crow26 <7tacson7@gmail.com>
2019-11-28 21:03:13 +0300

    Добавляет стартовый шаблон для создания проектов в стандартном стеке

---
+++ standart-template-project/src/js/modules/util.js
@@ -0,0 +1,13 @@
+/**
+* Скрипт для хранения констант, переменных и функций, которые используются в нескольких JavaScript файлах
+*/
+
+'use strict';
+
+(function () {
+  var ESC_KEY_CODE = 27;
+
+  window.util = {
+    ESC_KEY_CODE: ESC_KEY_CODE
+  };
+})();
